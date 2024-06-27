a76713ea36b57fa4109892d4dbad304825c52117
Rmunene123 <rosewanmun@gmail.com>
2024-06-28 02:31:33 +0300

    added function 2.0

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+function saturdayFun(activity= "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+    console.log(`This Saturday, I want to ${activity}!`)
+}
+saturdayFun();
+saturdayFun("bathe my dog"); 
+
+function mondayWork(activity= "go to the office") {
+    return `This Monday, I will ${activity}.`;
+    console.log(`This Monday, I will ${activity}.`)
+    
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective( msg = "You are *a hard worker*!") {
+    const innerFunction = function (special = "special") {
+        if(msg===`*`){
+            return `You are ${msg}a hard worker${msg}!`;
+        }else{
+           return `You are ${msg}a dedicated programmer${msg}!`
+        }
+      
+    };
+    return innerFunction;
+  }
